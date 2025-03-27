c6b11b338fb04ef3246ae0d45b81d02b4b3497b7
Murad <seferov.murad.98@gmail.com>
2025-03-27 14:53:18 +0400

    'Blog_models_create'

---
+++ blogs/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+from datetime import datetime
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name_plural  = 'Categories'
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField(blank=True, null=True)
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+    # author = models.ForeignKey('User', on_delete=models.CASCADE, null=True, blank=True)
+    Category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.title
+    
+
+class Comment(models.Model):
+    # author = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.post} / {self}'
